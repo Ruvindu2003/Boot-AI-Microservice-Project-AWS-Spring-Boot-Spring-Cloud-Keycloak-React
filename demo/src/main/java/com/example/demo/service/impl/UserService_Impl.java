55eccd207987dbcd48a2ee640f506e1fd0af4df9
Ruvindu <ruvindusharada22@gmail.com>
2025-09-28 11:40:41 +0530

    create t this  for the repsitory Layer and   service Layer user Register Logic

--- demo/src/main/java/com/example/demo/service/impl/UserService_Impl.java
+++ demo/src/main/java/com/example/demo/service/impl/UserService_Impl.java
@@ -2,6 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.dto.UserResponse;
 import com.example.demo.entity.RegisterequestEntity;
+import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,12 @@ public class UserService_Impl implements UserService {
 
     @Override
     public UserResponse register(RegisterequestEntity registerequest) {
+
+        UserEntity userEntity=new UserEntity();
+        userEntity.setEmail(registerequest.getEmail());
+        userEntity.setPassword(registerequest.getPassword());
+        userEntity.setName(registerequest.getFirstName());
+        userEntity.setLastName(registerequest.getLastName());
         return null;
     }
 }
