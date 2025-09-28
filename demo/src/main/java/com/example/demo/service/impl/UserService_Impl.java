3fd8c439cde97b1040026e80d2ea622134761953
Ruvindu <ruvindusharada22@gmail.com>
2025-09-28 11:33:29 +0530

    create the Service class user and Impl and  User controller and User Receponce Class

---
+++ demo/src/main/java/com/example/demo/service/impl/UserService_Impl.java
@@ -0,0 +1,22 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.UserResponse;
+import com.example.demo.entity.RegisterequestEntity;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService_Impl implements UserService {
+
+
+    @Override
+    public UserResponse getUserProfile(Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserResponse register(RegisterequestEntity registerequest) {
+        return null;
+    }
+}
