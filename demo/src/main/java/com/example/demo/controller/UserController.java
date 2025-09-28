6b42bd2176b00a75563a2d5840d9442d810a556e
Ruvindu <ruvindusharada22@gmail.com>
2025-09-28 13:27:07 +0530

    rgeister is ingration is ovee

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -11,15 +11,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-
+@RequiredArgsConstructor
 
 public class UserController {
     private  final  UserService userService;
 
-    public UserController( UserService userService1){
 
-        this.userService = userService1;
-    }
 
 
 
