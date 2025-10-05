72c3d57ed29b609b7b592613954058559571de60
Ruvindu <ruvindusharada22@gmail.com>
2025-10-05 14:45:54 +0530

    Add MongoDB configuration class to enable auditing

---
+++ aiService/aiService/src/main/java/com/fitnersse/aiService/config/MongoConfig.java
@@ -0,0 +1,14 @@
+package com.fitnersse.aiService.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+
+
+
+}
