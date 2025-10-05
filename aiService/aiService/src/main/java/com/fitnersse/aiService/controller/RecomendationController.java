e36529eb4fcef702723271915c975911dcdb59e0
Ruvindu <ruvindusharada22@gmail.com>
2025-10-05 14:53:10 +0530

    Add RecomendationController and Lombok @Builder annotation to Recomendation model

---
+++ aiService/aiService/src/main/java/com/fitnersse/aiService/controller/RecomendationController.java
@@ -0,0 +1,13 @@
+package com.fitnersse.aiService.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recomendations")
+@CrossOrigin
+
+public class RecomendationController {
+}
