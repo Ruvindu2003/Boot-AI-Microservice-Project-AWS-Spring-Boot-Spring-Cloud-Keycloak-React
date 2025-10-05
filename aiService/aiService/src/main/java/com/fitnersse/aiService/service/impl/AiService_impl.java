916f4a2822bad58bcc86254614a6b9494aa55dcf
Ruvindu <ruvindusharada22@gmail.com>
2025-10-05 20:31:56 +0530

    Add methods to AiRepositoory and AiService; implement user and activity recommendation retrieval in AiService_impl and RecomendationController

--- aiService/aiService/src/main/java/com/fitnersse/aiService/service/impl/AiService_impl.java
+++ aiService/aiService/src/main/java/com/fitnersse/aiService/service/impl/AiService_impl.java
@@ -1,4 +1,34 @@
 package com.fitnersse.aiService.service.impl;
 
-public class AiService_impl {
+import com.fitnersse.aiService.model.Recomendation;
+import com.fitnersse.aiService.repository.AiRepositoory;
+import com.fitnersse.aiService.service.AiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class AiService_impl implements AiService {
+
+    private final AiRepositoory aiRepositoory;
+
+
+    @Override
+    public Recomendation getAllActivityId(String activityId) {
+
+        Recomendation activityNotFound = aiRepositoory.findAllByActivityId(activityId)
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Activity not found"));
+
+        return activityNotFound;
+    }
+
+    @Override
+    public List<Recomendation> getAllUserRecomendation(String userId) {
+      return   aiRepositoory.findAllByUserId(userId);
+    }
 }
